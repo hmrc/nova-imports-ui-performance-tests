3e752dfa7ee37966b74f949bb1ade9e8a839ec7f
ardurojaye <101007648+adurojaye@users.noreply.github.com>
2026-03-30 15:01:02 +0100

    Format

--- src/test/scala/uk/gov/hmrc/perftests/example/BaseRequest.scala
+++ src/test/scala/uk/gov/hmrc/perftests/example/BaseRequest.scala
@@ -7,9 +7,9 @@ import uk.gov.hmrc.performance.conf.ServicesConfiguration
 
 trait BaseRequest extends ServicesConfiguration {
 
-  val baseUrl: String = baseUrlFor("nova-imports-notification-frontend")
-  val route: String   = "/nova-imports/start"
-  val authUrl: String  = baseUrlFor("auth-login-stub")
+  val baseUrl: String          = baseUrlFor("nova-imports-notification-frontend")
+  val route: String            = "/nova-imports/start"
+  val authUrl: String          = baseUrlFor("auth-login-stub")
   val authLoginStubUrl: String = s"$authUrl/auth-login-stub/gg-sign-in"
 
   val CsrfPattern = """<input type="hidden" name="csrfToken" value="([^"]+)""""
--- src/test/scala/uk/gov/hmrc/perftests/example/NovaSimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/example/NovaSimulation.scala
@@ -3,14 +3,14 @@ package uk.gov.hmrc.perftests.example
 import uk.gov.hmrc.performance.simulation.PerformanceTestRunner
 import uk.gov.hmrc.perftests.example.AuthLoginRequests._
 
-class NovaSimulation extends PerformanceTestRunner{
+class NovaSimulation extends PerformanceTestRunner {
 
   setup(
     "auth-baseline-simulation",
     "NoVA Imports Notification Baseline Simulation"
-  ) withRequests(
-      navigateToAuth,
-      authLogInAsIndividual
-    )
+  ) withRequests (
+    navigateToAuth,
+    authLogInAsIndividual
+  )
   runSimulation()
 }
