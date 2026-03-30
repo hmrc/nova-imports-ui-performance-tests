b200c255488904ec24cf2b1ccddc18bbe9e353f8
ardurojaye <101007648+adurojaye@users.noreply.github.com>
2026-03-30 15:17:21 +0100

    Cleanup and Copyright

--- src/test/scala/uk/gov/hmrc/perftests/nova/AuthLoginRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/nova/AuthLoginRequests.scala
@@ -1,4 +1,20 @@
-package uk.gov.hmrc.perftests.example
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.perftests.nova
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
@@ -13,7 +29,7 @@ object AuthLoginRequests extends BaseRequest {
       .check(saveCsrfToken())
       .check(regex("Authority Wizard").exists)
 
-  private val startPageUrl: String = s"$baseUrl$route"
+  private val startPageUrl: String = s"$baseUrl$route/start"
 
   val authLogInAsIndividual: HttpRequestBuilder =
     http("Login as Individual User")
@@ -22,9 +38,9 @@ object AuthLoginRequests extends BaseRequest {
       .formParam("csrfToken", csrfTokenExpr)
       .formParam("credentialStrength", "strong")
       .formParam("affinityGroup", "Individual")
+      .formParam("enrolment[0].name", "")
       .formParam("enrolment[0].taxIdentifier[0].name", "")
-      .formParam("enrolment[0].identifiers[0].name", "")
-      .formParam("enrolment[0].identifiers[0].value", "")
+      .formParam("enrolment[0].taxIdentifier[0].value", "")
       .check(status.is(303))
       .check(header("Location").is(startPageUrl))
 }
